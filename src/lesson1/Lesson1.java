7f96591ac2de7420397cad69971d8c857267a2d6
Lenovo <art.kazun@gmail.com>
2021-04-05 23:16:18 +0300

    Initial commit

---
+++ src/lesson1/Lesson1.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        for (int j = 1; j<11; j++){
+            for (int i = 1; i <11; i++){
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
