a2bdc761aaa7727117a7564c7b4d224737cb7479
Lenovo <art.kazun@gmail.com>
2021-04-07 23:29:10 +0300

    Homework

--- src/lesson2/Homework4.java
+++ src/lesson2/Homework4.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Homework4 {
     public static void main(String[] args) {
         Dog dog = new Dog();
+        System.out.println("Введите имя вашего тузика");
         Scanner write = new Scanner(System.in);
         dog.name = write.nextLine();
         createDog(dog.name);
     }
     static void createDog(String name){
         String[] massDog = new String[5];
-        Dog dog = new Dog();
         for (int i = 0;i<5;i++){
-            dog.age = (int) (Math.random()*10)+1;
-            System.out.println(name+"  "+dog.age);
+            massDog[i] = name + " "+ (int) (Math.random()*10)+1;
+            System.out.println(massDog[i]);
         }
     }
 }
